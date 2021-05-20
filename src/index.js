2db6dfc365603f24260c341944f9512e2d3ce773
AntonZubrytskyi <mranton19@gmail.com>
2021-05-20 12:58:20 +0300

    +

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import reportWebVitals from './reportWebVitals';
-import * as serviceWorker from './serviceWorker';
+
 
 
 
@@ -19,5 +19,6 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 // reportWebVitals();
+reportWebVitals();
 
-serviceWorker.unregister()
+
